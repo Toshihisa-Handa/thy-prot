edbcb230e60ff1c4e61c08b025a4cdf7e453ef6e
Toshi <waykiyoharu@gmail.com>
2020-10-29 12:13:02 +0900

    途中経過プッシュのため

--- index.php
+++ index.php
@@ -28,12 +28,24 @@
 
 
 
-
     <!-- アバウト -->
     <div class="about">
         <div class="contents content-head">
-        <h1>アバウト</h1>
-
+           <div class="about-left">
+               <div class="aboutTitle font-ll">About Us&nbsp; &nbsp; <img style='width:40px;' src='<?php echo get_template_directory_uri(); ?>/img/hana2.jpg ?>'></div>
+               <div class="aboutText font-m">私たちは、関東近郊に2物件を所有し、あらゆる層に低価格で<br>
+                                               住まいを提供する事業を行っています。<br>
+                                               私たちは、低価格であること、イコール、古く条件が悪いアパ<br>
+                                               ートではなく、立地条件がよい物件を選別し、手を加えること<br>
+                                               で安くてもよい住まいを提供できるよう事業をおこなっており<br>
+                                               ます。私たちは、関東近郊に2物件を所有し、あらゆる層に低<br>
+                                               価格で住まいを提供する事業を行っています。
+               </div>
+               <div class="more">more</div>
+            </div>
+            <div class="about-right">
+              <div class="rightImg"></div>
+           </div>
         </div>
 
 
