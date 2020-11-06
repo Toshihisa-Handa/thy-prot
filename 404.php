38f89f8308cc93268b5f7ef39d4f7d97465f6809
babahito <babahito@gmail.com>
2020-11-06 15:29:48 +0900

    試し

--- 404.php
+++
@@ -1,9 +0,0 @@
-<?php get_header(); ?>
-    <!-- ヘッダールート -->
-    <div class="headerRoute"></div>
-    <!-- ヘッダールートここまで -->
-
-    <div class="error_page">
-    <h1>記事がありません</h1>
-</div>
-<?php get_footer(); ?>
