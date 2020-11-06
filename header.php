28654ee64b607cd6d0a87340a2909d6d53dbf12e
Toshi <waykiyoharu@gmail.com>
2020-11-06 14:57:13 +0900

    headerのwpタグを一修正

--- header.php
+++ header.php
@@ -1,6 +1,5 @@
 <!DOCTYPE html>
 <html <?php language_attributes(); ?>>
-<?php wp_head(); ?>
 <head>
     <meta charset="<?php bloginfo( 'charset' ); ?>" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -14,7 +13,8 @@
     <link href="<?php echo get_template_directory_uri(); ?>/css/top.css" rel="stylesheet">
 
     
-
+<?php wp_head(); ?>
+</head>
 <body>
  <div class="container">
         <!-- ヘッダー -->
