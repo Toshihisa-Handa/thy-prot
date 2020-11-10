19d00320024c554a6ac820f15b0a21f05dc3f9a8
babahito <babahito@gmail.com>
2020-11-10 22:05:44 +0900

    TOP斜め修正中

--- index.php
+++ index.php
@@ -34,13 +34,9 @@
     </div>
     <div class="concept-right">
         <div class="conceptTitle font-32">About Us&nbsp; <img class='icon01' src='<?php echo get_template_directory_uri(); ?>/img/icon_01.jpg ?>/img/icon_01.jpg'></div>
-        <div class="conceptText font-15">私たちは、関東近郊に2物件を所有し、<br>
-            あらゆる層に低価格で住まいを提供する事業を行っています。<br>
-            私たちは、低価格であること、イコール、古く条件が悪いアパ<br>
-            ートではなく、立地条件がよい物件を選別し、手を加えること<br>
-            で安くてもよい住まいを提供できるよう事業をおこなっており<br>
-            ます。私たちは、関東近郊に2物件を所有し、あらゆる層に低<br>
-            価格で住まいを提供する事業を行っています。
+        <div class="conceptText font-15">私たちは、関東近郊に2物件を所有し、あらゆる層に低価格で住まいを提供する事業を行っています。<br>
+            私たちは、低価格であること、イコール、古く条件が悪いアパートではなく、立地条件がよい物件を選別し、手を加えることで安くてもよい住まいを提供できるよう事業をおこなっております。<br>
+            私たちは、関東近郊に2物件を所有し、あらゆる層に低価格で住まいを提供する事業を行っています。
         </div>
     </div>
     </div>
@@ -56,9 +52,8 @@
     </div>
     <div class="concept-right">
         <div class="conceptTitle font-32">One Team&nbsp; <img class='icon01' src='<?php echo get_template_directory_uri(); ?>/img/icon_01.jpg'></div>
-        <div class="conceptText font-15">私たちは、不動産や中小のリフォーム会社とタッグ<br>
-                                を組んで、お客様によりよい条件のアパートを提供<br>
-                                できるよう、密に連携をくんでとりくんでおります。
+        <div class="conceptText font-15">
+            私たちは、不動産や中小のリフォーム会社とタッグを組んで、お客様によりよい条件のアパートを提供できるよう、密に連携をくんでとりくんでおります。
         </div>
     </div>
     </div>
@@ -76,7 +71,7 @@
         <div class="propertyCard-box">
             <div class="pCard">
                 <div class="cardImg-box">
-                <img class='cardImg' src="<?php echo get_template_directory_uri(); ?>/img/property_02.jpg?>" alt="">
+                <img class='cardImg' src="<?php echo get_template_directory_uri(); ?>/img/top_05.png?>" alt="">
                 </div>
                 <div class="cardSentence">
                     <div class="cardTitle font-16">埼玉県草加市</div>
