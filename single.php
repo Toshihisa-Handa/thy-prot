52e0285aea9812965c25e9abbced3fc569c886af
Toshi <waykiyoharu@gmail.com>
2020-11-18 09:55:33 +0900

    single.phpのcss修正（メインのカード以外）

--- single.php
+++ single.php
@@ -61,26 +61,20 @@
             <?php if(have_posts()): while(have_posts()):
             the_post(); ?>
             
-            <div class="detailCard">
-                <div class="detailCard-top">
+            <div class="mainCard">
+                <div class="mainCard-left">
                 <?php if(has_post_thumbnail()): ?>
                     <?php $postthumb=wp_get_attachment_image_src(get_post_thumbnail_id()); ?>
-                <img class='mainImg' src="<?php echo $postthumb[0]; ?>">
+                <img class='main-blogImg' src="<?php echo $postthumb[0]; ?>">
                 <?php else: ?>
-                    <img class='mainImg' src="<?php echo get_template_directory_uri(); ?>/img/no_image.png">
+                    <img class='main-blogImg' src="<?php echo get_template_directory_uri(); ?>/img/no_image.png">
                 <?php endif; ?>
                 </div>
-                <div class="detailCard-bottom">
-                    <div class="dtitleBox">
+                <div class="mainCard-right">
                         <div class="mainCard-title font-20"><?php the_title(); ?></div>
-                        <div class="mainCard-tag-box">
-                        <?php the_tags('<ul><li class="mainCard-tag font-12">','</li><li class="mainCard-tag font-12">','</li></ul>'); ?>
-                            <div class="mainCard-time font-14 mt-10">更新日:<?php echo get_the_modified_date('Y/m/d'); ?></time></div>
-                        </div>
-                    </div>
-                    <div class="detailCard-text font-16 mt-30">
-                    <?php the_excerpt(); ?>
-                    </div>
+                        <div class="mainCard-tag font-16"><?php the_tags('タグ：', '  ', '<br />' ); ?></div>
+                        <div class="mainCard-time ">更新日:<?php echo get_the_modified_date('Y/m/d'); ?></div>
+                        <div class="mainCard-text font-16 "><?php the_content(); ?></div>
 
                     <div class="contact">
                     <div class="contact-area">
@@ -94,58 +88,12 @@
 
 
         <!-- 関連記事 -->
-        <div class="commonTitleBox mt-30">
-            <img src="<?php echo get_template_directory_uri(); ?>/img/icon_02.jpg" alt="">
-            <div class="commonTitle font-20">関連記事</div>
-        </div>
-        <div class="related">
-            <?php if(has_category()){
-                    $cats=get_the_category();
-                    $catkwds=array();
-                    foreach($cats as $cat){
-                        $catkwds[]=$cat->term_id;
-                    }
-                }
-            ?>
-            <?php 
-            $myposts=get_posts(array(
-                'post_type'=>'post',
-                'posts_per_page'=>'3',
-                'post__not_in'=>array($post->ID),
-                'category__in'=>$catkwds,
-                'orderby'=>'rand'
-            ));
-            if($myposts): ?>
-        <!-- 関連カード -->
-        <?php foreach($myposts as $post):
-                setup_postdata($post); ?>
-            <div class="relatedCard">
-                    <?php if(has_post_thumbnail()): ?>
-                        <?php $postthumb=wp_get_attachment_image_src(get_post_thumbnail_id()); ?>
-                    <img class='blogImg' src="<?php echo $postthumb[0]; ?>">
-                    <?php else: ?>
-                        <img class='blogImg' src="<?php echo get_template_directory_uri(); ?>/img/no_image.png">
-                    <?php endif; ?>
-                <div class="blogTitleBox">
-                    <a href="<?php the_permalink(); ?>">
-                        <div class="blogCard-title font-16 "><?php the_title(); ?></div>
-                    </a>
-                        <div class="blogCard-tag-box">
-                            <div class="blogCard-tag font-12 "><?php the_tags('<ul><li class="mainCard-tag font-12">','</li><li class="mainCard-tag font-12">','</li></ul>'); ?></div>
-                            <div class="blogCard-time font-10 mt-10">更新日:<?php echo get_the_modified_date('Y/m/d'); ?></time></div>
-                        </div>
-                </div>
-                <div class="blogCard-text font-12">
-                    <?php the_excerpt(); ?>
-                </div>
-            </div>
-        <?php endforeach; ?>
-        <!-- 関連カードここまで -->
-        <?php wp_reset_postdata();
-            endif; ?>
-        </div>
-        </div>
+        <?php get_template_part('related', 'card') ?>
+
         <!--関連記事ここまで-->
+
+        </div>
+
         <!-- コンテンツライト -->
         <div class="bcontentRight">
                 <?php get_sidebar(); ?>
