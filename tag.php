9c8b53ba0e55220846078318e00ebf6642f156a3
Toshi <waykiyoharu@gmail.com>
2020-11-19 16:19:44 +0900

    ハンバーガーメニューにホバー追加,tag.pnpのハンバーガーメニューのリンク追加

--- tag.php
+++ tag.php
@@ -9,28 +9,15 @@ Template Name: BLOG
 
        <!-- レスポンシブヘッダーリンク -->
        <div id='reHeader-wrapper' class="reHeader-wrapper hidden">
-        <div class="reHeader-innerwrapper">
-           <div class="reHeader">
-           <ul class='reNav'>
-               <li class="reNavs">
-                 <a href="#"><div class="font-28 c-white">HOME</div></a> 
-               </li>
-               <li class="reNavs">
-                <a href="#"> <div class="font-28 c-white">COMPANY</div></a> 
-               </li>
-               <li class="reNavs">
-                <a href="#"><div class="font-28 c-white">PROPERTY</div></a> 
-               </li>
-               <li class="reNavs">
-                <a href="#"><div class="font-28 c-white">BLOG</div></a> 
-               </li>
-               <li class="reNavs">
-                <a href="#"><div class="font-28 c-white">CONTACT</div></a> 
-               </li>
-           </ul>
-           </div>
-           </div>
-       </div>
+<div class="reHeader-innerwrapper">
+    <?php wp_nav_menu(array(
+    'theme_location'=>'sitenav',
+    'container'=>'div',
+    'container_class'=>'reHeader',
+    'container_id'=>'hidemenu'
+    )); ?>
+</div>
+</div>
       <!-- レスポンシブヘッダーリンクここまで -->
 
 
